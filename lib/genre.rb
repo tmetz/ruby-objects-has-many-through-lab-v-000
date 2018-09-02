258e68e86186274c15a6269b92225f164a9cdd08
Tammy Metz <tammy@tammymetz.com>
2018-09-02 15:23:24 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -11,4 +11,14 @@ class Genre
     @name = name
     @@all << self
   end
+
+  def new_song(song_name, artist)
+    song = Song.new(song_name, artist, self)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.genre == self
+    end
+  end
 end
